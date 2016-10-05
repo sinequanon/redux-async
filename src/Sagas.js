6c556dd17d9114861554c3267f39c2faaa291ba2
Rowell Sotto <github@sotto.org>
2016-10-04 22:38:27 -0700

    Slight refactor for sagas

--- src/Sagas.js
+++ src/Sagas.js
@@ -1,27 +1,33 @@
 import { takeEvery } from 'redux-saga'
-import { put, call, take, select } from 'redux-saga/effects'
+import { put, call, take, select, fork } from 'redux-saga/effects'
 import { FETCH_POSTS, requestPosts, receivePosts } from './Actions'
 import axios from 'axios'
 
 function *fetchPosts () {
   const state = yield select()
-  console.log('state : %o', state)
+  console.log('state : %O', state)
+  console.log('state.selectSubreddit : %s', state.selectSubreddit);
   yield put(requestPosts(state.selectSubreddit))
   const { data } = yield call(axios.get, `http://www.reddit.com/r/${state.selectSubreddit}.json`)
-  console.log('data', data);
-  yield put(receivePosts(state.selectSubreddit, data))
+  return { data, selectSubreddit: state.selectSubreddit };
 }
 
 function *watchFetchPosts () {
-  yield *takeEvery(FETCH_POSTS, fetchPosts)
+  // yield *takeEvery(FETCH_POSTS, fetchPosts)
+  while (yield(take(FETCH_POSTS))) {
+    const { data, selectSubreddit } = yield(call(fetchPosts))
+    console.log('wfp data', data);
+    console.log('wfp selectSubreddit', selectSubreddit);
+    yield put(receivePosts(selectSubreddit, data))
+  }
 }
 
 function *watchAndLog () {
   while (true) {
     const action = yield take('*')
     const { state } = yield select()
-    console.log('action : %o', action)
-    console.log('state : %o', state)
+    console.log('action : %O', action)
+    console.log('state : %O', state)
   }
 }
 
